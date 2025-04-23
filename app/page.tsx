d577d474471e8c528d3d56b8305160e5f20ce6be
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-04-23 11:21:38 +0200

    correct package referenced

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 //
-// This source file is part of the Stanford Biodesign Digital Health Spezi Firebase Remote Notifications open-source project
+// This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
 //
-// SPDX-FileCopyrightText: 2025 Stanford University and the project authors (see CONTRIBUTORS.md)
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
 //
 // SPDX-License-Identifier: MIT
 //
 
 import Image from 'next/image'
+import { generateGreeting } from '@stanfordbdhg/spezi-frn'
 
 export default function Home() {
+  const greeting = generateGreeting()
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center">
@@ -18,9 +21,7 @@ export default function Home() {
           width={634}
           height={235}
         />
-        <h1 className="mt-4 text-center text-3xl">
-          Welcome to the Spezi Firebase Remote Notifications
-        </h1>
+        <h1 className="mt-4 text-center text-3xl">{`${greeting.message} to the ${greeting.project}`}</h1>
       </div>
     </div>
   )
