fbda8f3ad9653627787934aa9a503e73eaec555b
Paul Goldschmidt <mail@paul-goldschmidt.de>
2025-04-23 11:21:38 +0200

    correct package referenced

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@
 //
 
 import Image from 'next/image'
-import { generateGreeting } from '@stanfordbdhg/example-package'
+import { generateGreeting } from '@stanfordbdhg/spezi-frn'
 
 export default function Home() {
   const greeting = generateGreeting()
