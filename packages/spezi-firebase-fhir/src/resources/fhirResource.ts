1fe8d7789979429645a6974db7f541674625c16b
Muhamed Skenderi <159011389+muhamedsk@users.noreply.github.com>
2025-09-25 18:00:47 +0200

    [FHIR] Practitioner and Coverage FHIR Resources (#37)
    # *[FHIR] Practitioner and Coverage FHIR Resources*
    
    ## :recycle: Current situation & Problem
    Adds Coverage and Practitioner resource types
    
    
    ## :gear: Release Notes
    *Add a bullet point list summary of the feature and possible migration
    guides if this is a breaking change so this section can be added to the
    release notes.*
    *Include code snippets that provide examples of the feature implemented
    or links to the documentation if it appends or changes the public
    interface.*
    
    
    ## :books: Documentation
    *Please ensure that you properly document any additions in conformance
    to [Spezi Documentation
    Guide](https://github.com/StanfordSpezi/.github/blob/main/DOCUMENTATIONGUIDE.md).*
    *You can use this section to describe your solution, but we encourage
    contributors to document your reasoning and changes using in-line
    documentation.*
    
    
    ## :white_check_mark: Testing
    *Please ensure that the PR meets the testing requirements set by CodeCov
    and that new functionality is appropriately tested.*
    *This section describes important information about the tests and why
    some elements might not be testable.*
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- packages/spezi-firebase-fhir/src/resources/fhirResource.ts
+++ packages/spezi-firebase-fhir/src/resources/fhirResource.ts
@@ -27,11 +27,14 @@ import { untypedPatientSchema } from './patient.js'
 import { untypedQuestionnaireSchema } from './questionnaire.js'
 import { untypedQuestionnaireResponseSchema } from './questionnaireResponse.js'
 import { untypedServiceRequestSchema } from './serviceRequest.js'
+import { untypedPractitionerSchema } from './practitioner.js'
+import { untypedCoverageSchema } from './coverage.js'
 
 export const fhirResourceSchema: ZodType<FhirResource> = z.lazy(() =>
   z.discriminatedUnion('resourceType', [
     untypedAllergyIntoleranceSchema,
     untypedAppointmentSchema,
+    untypedCoverageSchema,
     untypedDeviceSchema,
     untypedDiagnosticReportSchema,
     untypedEncounterSchema,
@@ -46,6 +49,7 @@ export const fhirResourceSchema: ZodType<FhirResource> = z.lazy(() =>
     untypedMedicationStatementSchema,
     untypedOrganizationSchema,
     untypedPatientSchema,
+    untypedPractitionerSchema,
     untypedQuestionnaireSchema,
     untypedQuestionnaireResponseSchema,
     untypedServiceRequestSchema,
---
+++ packages/spezi-firebase-fhir/test/resources/coverage.test.ts
@@ -0,0 +1,24 @@
+// This source file is part of the Stanford Biodesign Digital Health Spezi Firebase Remote Notifications open-source project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import fs from 'fs'
+import { jsonStringifyDeterministically } from './testHelpers.js'
+import { FhirCoverage } from '../../src/index.js'
+
+describe('Coverage Resource', () => {
+  it('should validate FHIR Coverages from coverages.json', () => {
+    const data = fs.readFileSync('test/resources/coverages.json', 'utf-8')
+    const decodedJson = JSON.parse(data)
+
+    Object.values(decodedJson).forEach((jsonValue: unknown) => {
+      const parsedResource = FhirCoverage.parse(jsonValue).value
+      expect(jsonStringifyDeterministically(jsonValue)).toBe(
+        jsonStringifyDeterministically(parsedResource),
+      )
+    })
+  })
+})
