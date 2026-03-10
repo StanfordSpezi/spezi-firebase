afba65bdb417900b5a3a34abd54fc47ce55858c4
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-10 18:17:33 +0100

    lint:fix

--- packages/spezi-firebase-utils/src/helpers/validatedOnCall.ts
+++ packages/spezi-firebase-utils/src/helpers/validatedOnCall.ts
@@ -3,16 +3,25 @@
 // SPDX-FileCopyrightText: 2026 Stanford University and the project authors (see CONTRIBUTORS.md)
 // SPDX-License-Identifier: MIT
 
-import { logger } from "firebase-functions/v2";
+import { logger } from 'firebase-functions/v2'
 import {
   onCall,
   type CallableRequest,
   HttpsError,
   type CallableOptions,
-} from "firebase-functions/v2/https";
-import { z, type ZodType } from "zod/v4";
+} from 'firebase-functions/v2/https'
+import { z, type ZodType } from 'zod/v4'
 
-export const validatedOnCall = <Schema extends ZodType, Return>(
+/**
+ *
+ * @param schema
+ * @param handler
+ * @param options
+ */
+export /**
+        *
+        */
+const validatedOnCall = <Schema extends ZodType, Return>(
   schema: Schema,
   handler: (request: CallableRequest<z.output<Schema>>) => Promise<Return>,
   options: CallableOptions = {},
@@ -24,22 +33,28 @@ export const validatedOnCall = <Schema extends ZodType, Return>(
     },
     async (request) => {
       try {
-        const validatedData = schema.parse(request.data);
+        /**
+         *
+         */
+        const validatedData = schema.parse(request.data)
+        /**
+         *
+         */
         const validatedRequest: CallableRequest<z.output<Schema>> = {
           ...request,
           data: validatedData,
-        };
-        return await handler(validatedRequest);
+        }
+        return await handler(validatedRequest)
       } catch (error) {
-        logger.error("Function error:", error);
+        logger.error('Function error:', error)
         if (error instanceof z.ZodError) {
           throw new HttpsError(
-            "invalid-argument",
-            "Invalid request data",
+            'invalid-argument',
+            'Invalid request data',
             error.issues,
-          );
+          )
         }
-        throw error;
+        throw error
       }
     },
-  );
+  )
